851ff94a335e0ac5f739f319fcbc1d2a8afdc931
Casta16m <scastaner459@boscdelacoma.cat>
2024-01-15 15:16:29 +0100

    Nova versio desencriptar el PDF

--- ClientReceive/ClientReceive/Program.cs
+++ ClientReceive/ClientReceive/Program.cs
@@ -9,7 +9,6 @@ class Program
     static TcpClient client;
     static NetworkStream stream;
     static StreamReader reader;
-    static StreamWriter writer;
 
     static void Main()
     {
@@ -22,7 +21,11 @@ class Program
             string clientAddress = ((System.Net.IPEndPoint)client.Client.LocalEndPoint).Address.ToString();
             Console.WriteLine($"Cliente ({clientAddress}) conectado al servidor de chat.");
 
-            Thread receiveThread = new Thread(ReceiveMessages);
+            // Obtener el flujo de red del servidor
+            NetworkStream stream = client.GetStream();
+
+            // Iniciar un hilo para la lectura de datos del servidor
+            Thread receiveThread = new Thread(() => ReceiveMessages(stream));
             receiveThread.Start();
         }
         catch (Exception ex)
@@ -33,58 +36,13 @@ class Program
 
     static void ConnectToServer()
     {
-        client.Connect("127.0.0.1", 12345);
+        client.Connect("localhost", 41305);
         stream = client.GetStream();
         reader = new StreamReader(stream, Encoding.ASCII);
-        writer = new StreamWriter(stream, Encoding.ASCII);
     }
 
-    static void ReceiveMessages()
+    static void ReceiveMessage()
     {
-        try
-        {
-            while (true)
-            {
-                string message = ReadMessage(reader);
-                if (message != null)
-                {
-                    
 
-                    //Console.Write(message);
-                    // Procesa el mensaje recibido
-                }
-                else
-                {
-                    // Se produjo un error en la lectura del mensaje
-                    break;
-                }
-            }
-        }
-        catch (IOException ex)
-        {
-            Console.WriteLine($"Error de lectura: {ex.Message}");
-        }
-    }
-
-
-
-    static string ReadMessage(StreamReader reader)
-    {
-        try
-        {
-            string receivedMessage = reader.ReadLine();
-            if (receivedMessage != null)
-            {
-                // Reemplazar saltos de línea para que se muestren correctamente en la consola
-                receivedMessage = receivedMessage.Replace("\n", Environment.NewLine);
-            }
-            return receivedMessage;
-        }
-        catch (IOException ex)
-        {
-            // Maneja el error de lectura, como una desconexión inesperada del cliente.
-            Console.WriteLine($"Error de lectura: {ex.Message}");
-            return null;
-        }
     }
 }
